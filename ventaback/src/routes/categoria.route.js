76ad40a8b00291032e55895cc46959f07ad54fec
= <=>
2026-08-23 12:27:45 -0300

    subiendo

--- ventaback/src/routes/categoria.route.js
+++ ventaback/src/routes/categoria.route.js
@@ -3,7 +3,7 @@ const categoriaCtrl = require('../controllers/categoria.controller');
 const requiereAdministrador = require('../middleware/auth.middleware');
 
 const router = express.Router();
-router.get('/', requiereAdministrador, categoriaCtrl.getCategorias);
-router.post('/', requiereAdministrador, categoriaCtrl.createCategoria);
+router.get('/', categoriaCtrl.getCategorias);
+router.post('/', categoriaCtrl.createCategoria);
 
 module.exports = router;
